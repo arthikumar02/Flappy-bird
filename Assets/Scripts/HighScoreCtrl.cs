88152c3ccfa8826d585929d9b3ce7bb51f88f7b9
DESKTOP-OQB2LKG\dev <ibc@proyuga.tech>
2023-06-29 12:10:23 +0530

    ﻿Importing TextMeshPro Assets

--- Assets/Scripts/HighScoreCtrl.cs
+++ Assets/Scripts/HighScoreCtrl.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SumanFeatureIsntGood : MonoBehaviour
+public class HighScoreCtrl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         print("bye");  
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
