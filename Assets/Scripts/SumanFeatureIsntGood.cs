d1ef8a1b40824d224ce56ea1c94190156cf03fdf
DESKTOP-OQB2LKG\dev <ibc@proyuga.tech>
2023-06-29 10:15:50 +0530

    Summan-Commit-2

--- Assets/Scripts/SumanFeatureIsntGood.cs
+++ Assets/Scripts/SumanFeatureIsntGood.cs
@@ -7,7 +7,7 @@ public class SumanFeatureIsntGood : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("bye");  
     }
 
     // Update is called once per frame
