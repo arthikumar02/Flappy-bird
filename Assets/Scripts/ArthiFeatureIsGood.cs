20bbaf272717dd89c3c5a9d3e4396d7eb8be0922
DESKTOP-IMKU9CD\dev <ibc@proyuga.tech>
2023-06-29 10:14:33 +0530

    arthi-Commit-2

--- Assets/Scripts/ArthiFeatureIsGood.cs
+++ Assets/Scripts/ArthiFeatureIsGood.cs
@@ -7,7 +7,7 @@ public class ArthiFeatureIsGood : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Hello");
     }
 
     // Update is called once per frame
